2d5a46f66946bad218dbfbc4f87e7b070d673936
haejin <luckyhaejin1126@gmail.com>
2023-11-01 00:07:25 +0900

    page routing - router2

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,32 @@
+import {useNavigate, useSearchParams} from "react-router-dom";
 const Edit = () => {
-    return <div>
-        <h1>Edit</h1>
-        <p>이곳은 일기 수정 페이지입니다.</p>
-    </div>
+    const navigate = useNavigate();
+    const [searchParam, setSearchParams] = useSearchParams();
+    const id = searchParam.get('id');
+    console.log("id: ", id);
+
+    const mode = searchParam.get('mode');
+    console.log("mode", mode); 
+return (
+        <div>
+            <h1>Edit</h1>
+            <p>이곳은 일기 수정 페이지입니다.</p>
+            <button onClick ={() => setSearchParams({who: "haejin"})}>QS 바꾸기</button>
+            <button 
+                onClick={() => { 
+                    navigate("/home");
+                }}
+            >
+                HOME으로 가기
+            </button>
+            <button 
+                onClick={() => { 
+                    navigate(-1);
+                }}
+            >
+                뒤로 가기
+            </button>
+        </div>
+    );  
 }
 export default Edit;
