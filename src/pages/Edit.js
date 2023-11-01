17c9991b7d508da2160d604e9ce8498a598f9539
haejin <luckyhaejin1126@gmail.com>
2023-11-01 21:47:56 +0900

    font, layout, image assets, common component(MyButton.js, MyHeader.js)

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,31 +1,9 @@
-import {useNavigate, useSearchParams} from "react-router-dom";
 const Edit = () => {
-    const navigate = useNavigate();
-    const [searchParam, setSearchParams] = useSearchParams();
-    const id = searchParam.get('id');
-    console.log("id: ", id);
 
-    const mode = searchParam.get('mode');
-    console.log("mode", mode); 
 return (
         <div>
             <h1>Edit</h1>
             <p>이곳은 일기 수정 페이지입니다.</p>
-            <button onClick ={() => setSearchParams({who: "haejin"})}>QS 바꾸기</button>
-            <button 
-                onClick={() => { 
-                    navigate("/home");
-                }}
-            >
-                HOME으로 가기
-            </button>
-            <button 
-                onClick={() => { 
-                    navigate(-1);
-                }}
-            >
-                뒤로 가기
-            </button>
         </div>
     );  
 }
