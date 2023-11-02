697f3fe7f68b82237d9b4b4b9543270a6480fb92
haejin <luckyhaejin1126@gmail.com>
2023-11-02 23:30:19 +0900

    Select box (in order of date, in order of emotion), write new day button, emotion image, date, content, modify button

--- src/App.js
+++ src/App.js
@@ -31,10 +31,42 @@ const reducer = (state, action) => {
 }
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext= React.createContext();
+const dummyData = () => [
+  {
+    id: 1,
+    emotion: 1,
+    content: "오늘의 일기 1번",
+    date:1698925849285
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "오늘의 일기 2번",
+    date:1698925849286
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "오늘의 일기 3번",
+    date:1698925849287
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "오늘의 일기 4번",
+    date:1698925849288
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "오늘의 일기 5번",
+    date:1698925849289
+  }
+];
 function App() {
-  const [data, dispatch] = useReducer(reducer,[]);
+  const [data, dispatch] = useReducer(reducer,dummyData());
   const dataId = useRef(0);
-
+  console.log(new Date().getTime());
   const onCreate = (date,content,emotion) => {
     dispatch({
       type:"CREATE",
